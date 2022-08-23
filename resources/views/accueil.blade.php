4a5c8dd94902c1f18068f444f450dacb72485f07
fallougeekeralafunti <fallou.toure@uadb.edu.sn>
2022-08-23 17:04:37 +0200

    services

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -129,20 +129,31 @@
             <span>01</span>
             <h4>Bureau du recouvrement</h4>
             <p>
-                le bureau du recouvrement est un service qui permet d'enregistrer de votre parcelle.
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Enregistrement de votre parcelle</a></li>
+                </ul>
             </p>
           </div>
 
           <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="100">
             <span>02</span>
             <h4>Repellat Nihil</h4>
-            <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+            <p>
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Etablissement de l'extrait de plan</a></li>
+                </ul>
+            </p>
           </div>
 
           <div class="col-lg-4 col-md-6 content-item" data-aos="fade-up" data-aos-delay="200">
             <span>03</span>
             <h4> Ad ad velit qui</h4>
-            <p>Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt debitis quo vel nam quis</p>
+            <p>
+                <ul>
+                    <li><a class="nav-link scrollto active" href="#hero">Deliberations</a></li>
+                    <li><a class="nav-link scrollto active" href="#hero">Contrat de bail</a></li>
+                </ul>
+            </p>
           </div>
         </div>
 
@@ -160,34 +171,20 @@
         <div class="row">
           <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
             <div class="icon"><i class="bi bi-chat-left-dots"></i></div>
-            <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+            <h4 class="title"><a href="">Bureau du recouvrement</a></h4>
+            <p class="description">le bureau du recouvrement est un service qui permet d'enregistrer de votre parcelle.
+            </p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
             <div class="icon"><i class="bi bi-bounding-box"></i></div>
-            <h4 class="title"><a href="">Dolor Sitema</a></h4>
-            <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+            <h4 class="title"><a href="">Bureau du Cadastre</a></h4>
+            <p class="description">le bureau du recouvrement est un service qui permet d'enregistrer de votre parcelle.</p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
             <div class="icon"><i class="bi bi-globe"></i></div>
-            <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-            <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-          {{-- </div>
-          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
-            <div class="icon"><i class="bi bi-broadcast"></i></div>
-            <h4 class="title"><a href="">Magni Dolores</a></h4>
-            <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-          </div>
-          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
-            <div class="icon"><i class="bi bi-brightness-high"></i></div>
-            <h4 class="title"><a href="">Nemo Enim</a></h4>
-            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-          </div>
-          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
-            <div class="icon"><i class="bi bi-calendar2-week"></i></div>
-            <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-            <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
-          </div> --}}
+            <h4 class="title"><a href="">Bureau du Domaine</a></h4>
+            <p class="description">le bureau du recouvrement est un service qui permet d'enregistrer de votre parcelle.</p>
+
         </div>
 
       </div>
